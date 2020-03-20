06a72a94115bfcf6b54bf94a2a3bf363b38d0e74
Simon Bowly <simon.bowly@gmail.com>
2020-03-20 14:17:04 +1100

    Use module level loggers and add log configuration to solver CLI.

--- examples/crossdock/solver.py
+++ examples/crossdock/solver.py
@@ -5,13 +5,35 @@ import click
 import crossdock.instance
 import crossdock.model
 
-logging.basicConfig(level=logging.WARNING)
+
+LOG_LEVELS = [level.lower() for level in logging._nameToLevel if level != "NOTSET"]
+
+
+def setup_console_logging(logger, level):
+    # From the cookbook
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
 
 @click.command()
 @click.argument("file-path", type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    "--log-level",
+    type=click.Choice(LOG_LEVELS, case_sensitive=False),
+    default="warning",
+)
 @click.option("--threads", type=int, default=None)
-def run(file_path, threads):
+def run(file_path, log_level, threads):
+    setup_console_logging(
+        logger=logging.getLogger("crossdock"),
+        level=logging._nameToLevel[log_level.upper()],
+    )
     instance = crossdock.instance.read_json(file_path)
     model = crossdock.model.construct_model(instance)
     solution = crossdock.model.solve_model(model, threads=threads)
@@ -20,11 +42,3 @@ def run(file_path, threads):
 
 
 run()
-
-
-# instance = generate_random_instance(19675, 25, 3)
-# model = construct_model(instance)
-# solution = solve_model(model)
-
-# print(instance)
-# print(solution)
